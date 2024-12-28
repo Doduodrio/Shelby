549352d8bacd5dd5ca89dcfdca0c6fb7bf07fac8
Doduodrio <132383877+Doduodrio@users.noreply.github.com>
2024-12-28 00:29:58 -0800

    Add datetime function and add_word command

---
+++ main.py
@@ -0,0 +1,77 @@
+# Shelby
+
+from dotenv import load_dotenv
+import datetime
+import json
+import os
+
+from discord import app_commands
+import discord
+
+load_dotenv()
+TOKEN = os.getenv('BOT_TOKEN') # add the bot token in a .env file
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+client = discord.Client(
+    activity=discord.Game(name='Pokémon Silver'),
+    intents=intents
+)
+tree = app_commands.CommandTree(client)
+
+def now():
+    # returns current timestamp
+    time = datetime.datetime.now()
+    date = [time.month, time.day, time.year, time.hour, time.minute, time.second]
+    for i in range(len(date)):
+        date[i] = str(date[i])
+        if len(date[i])==1: date[i] = '0' + date[i]
+    return '[{}-{}-{} {}:{}:{}]'.format(*date)
+
+@client.event
+async def on_ready():
+    guilds = '\n - '.join([f'{guild.name} (id: {guild.id})' for guild in client.guilds])
+    print(f'{client.user} is active in the following guilds:')
+    print(f' - {guilds}\n')
+    me = client.get_user(587040390603866122)
+    await me.send('Yadoyadoking...!')
+    print('Shelby sent a DM to doduodrio (id: 587040390603866122) upon activating!')
+
+# @client.event
+# async def on_message(message):
+#     # if it's your own message, don't react
+#     if message.author == client.user:
+#         return
+#     # otherwise, react
+#     msg = message.content.lower() # "hElLo!" => "hello!"
+
+@tree.command(description='Add a word to your dictionary')
+@app_commands.describe(
+    word='The word or phrase to add.',
+    definition='The definition of the word or phrase being added.'
+)
+async def add_word(i: discord.Interaction, word: str, definition: str):
+    # load pre-existing words from file
+    try:
+        with open(f'{i.user.name}.json', 'r') as file:
+            dictionary = json.load(file)
+    except:
+        dictionary = {}
+    
+    # add new definition
+    if word in dictionary:
+        dictionary[word].append(definition)
+    else:
+        dictionary[word] = [definition]
+    
+    # update file with new word
+    with open(f'{i.user.name}.json', 'w') as file:
+        file.write(json.dumps(dictionary, indent=4))
+
+    await i.response.send_message(f'The word `{word} has been added to your dictionary!`', ephemeral=True)
+    print(f'{now()} [{i.user.name}] Add "{word}" to dictionary')
+
+# client.run(TOKEN)
+
+print(now())
