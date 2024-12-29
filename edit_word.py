7f2b864ca243655b3eed81678df83456059c5524
Doduodrio <132383877+Doduodrio@users.noreply.github.com>
2024-12-29 14:00:56 -0800

    Merge word and definition editing into one modal

--- edit_word.py
+++ edit_word.py
@@ -18,15 +18,17 @@ def get_dictionary(user):
     except:
         return {}
 
-class TextInput(discord.ui.Modal):
-    def __init__(self, title: str, input_name: str, callback):
-        super().__init__(title=title)
-        self.input = discord.ui.TextInput(label=input_name)
-        self.add_item(self.input)
+class WordEditor(discord.ui.Modal):
+    def __init__(self, word: str, definition: str, callback):
+        super().__init__(title=f'Editing word {word}')
+        self.word_input = discord.ui.TextInput(label='Word', default=word)
+        self.add_item(self.word_input)
+        self.definition_input = discord.ui.TextInput(label='Definition', default=definition)
+        self.add_item(self.definition_input)
         self.callback = callback # function to call when input form is submitted
     
     async def on_submit(self, i: discord.Interaction):
-        await self.callback(i, self.input.value)
+        await self.callback(i, self.word_input.value, self.definition_input.value)
 
 class EditMenu(discord.ui.View):
     def __init__(self, user, word):
@@ -36,6 +38,7 @@ class EditMenu(discord.ui.View):
     
     def get_embed(self):
         self.dictionary = get_dictionary(self.user)
+        self.definition = self.dictionary[self.word]['definition']
 
         embed = discord.Embed(
             color = discord.Color.dark_teal(),
@@ -44,40 +47,51 @@ class EditMenu(discord.ui.View):
             timestamp = datetime.datetime.now()
         )
         embed.add_field(name='Word', value=self.word, inline=False)
-        embed.add_field(name='Definition', value=self.dictionary[self.word], inline=False)
+        embed.add_field(name='Definition', value=self.definition, inline=False)
 
         return embed
     
     async def send(self, i: discord.Interaction):
         await i.response.send_message(embed=self.get_embed(), view=self)
-        self.interaction = i
+        self.original_response = await i.original_response()
     
-    async def edit_word(self, i: discord.Interaction, new_word: str):
-        if new_word in self.dictionary.keys():
+    async def edit_word(self, i: discord.Interaction, new_word: str, new_definition: str):
+        if new_word in self.dictionary.keys() and new_word != self.word:
+            # two words cannot share the same key
             await i.response.send_message(f'The word `{new_word}` already exists in your dictionary.', ephemeral=True)
             print(f'{now()} [{i.user.name}] edit_word: tried to change word "{self.word}" to pre-existing word "{new_word}"')
         else:
-            await i.response.send_message(f'The word `{self.word}` has been changed to `{new_word}`.', ephemeral=True)
-            print(f'{now()} [{i.user.name}] edit_word: changed word "{self.word}" to "{new_word}')
-
-            # move old word data to a new key
+            # delete old entry and make new entry with new data
             old_word = self.dictionary.pop(self.word)
             self.dictionary[new_word] = {
                 'word': new_word,
-                'definition': old_word['definition'],
+                'definition': new_definition,
                 'date added': old_word['date added']
             }
             self.word = new_word
+            self.definition = new_definition
 
-            # update file with new word
+            # update file with new data
             with open(f'dictionaries/{self.user}.json', 'w') as file:
                 file.write(json.dumps(self.dictionary, indent=4))
             
-            # update original response with new word
-            response = await self.interaction.original_response()
-            await response.edit(emded=self.get_embed(), view=self)
+            # update original response with new data
+            await self.original_response.edit(embed=self.get_embed(), view=self)
+
+            # send confirmation message
+            await i.response.send_message(f'Your changes have been saved.', ephemeral=True)
+            print(f'{now()} [{i.user.name}] edit_word: changed word')
+            print(f'    old: "{old_word["word"]}" with definition "{old_word["definition"]}"')
+            print(f'    new: "{new_word}" with definition "{new_definition}"')
 
     @discord.ui.button(style=discord.ButtonStyle.primary, label='Edit Word')
     async def edit_word_button(self, i: discord.Interaction, b: discord.ui.Button):
-        modal = TextInput(f'Editing word {self.word}', 'New word', self.edit_word)
+        modal = WordEditor(self.word, self.dictionary[self.word]['definition'], self.edit_word)
         await i.response.send_modal(modal)
+    
+    async def delete_word(self, i: discord.Interaction):
+        pass
+    
+    @discord.ui.button(style=discord.ButtonStyle.danger, label='Delete Word')
+    async def delete_word_button(self, i: discord.Interaction, b: discord.ui.Button):
+        pass
