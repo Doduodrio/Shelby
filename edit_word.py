a5315425788e3283542532cf5deb6bd73d195658
Doduodrio <132383877+Doduodrio@users.noreply.github.com>
2024-12-29 17:06:34 -0800

    Move now() and get_dictionary() functions to separate file

--- edit_word.py
+++ edit_word.py
@@ -1,22 +1,7 @@
+from functions import *
 import datetime
 import discord
 import json
-
-def now():
-    # returns current timestamp
-    time = datetime.datetime.now()
-    date = [time.month, time.day, time.year, time.hour, time.minute, time.second]
-    for i in range(len(date)):
-        date[i] = str(date[i])
-        if len(date[i])==1: date[i] = '0' + date[i]
-    return '[{}-{}-{} {}:{}:{}]'.format(*date)
-
-def get_dictionary(user):
-    try:
-        with open(f'dictionaries/{user}.json', 'r') as file:
-            return json.load(file)
-    except:
-        return {}
 
 class WordEditor(discord.ui.Modal):
     def __init__(self, word: str, definition: str, callback):
